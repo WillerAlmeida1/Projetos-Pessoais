78ec96ea3cc7867f7a58f3b9d05456d3d94f0bc0
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-03-29 21:43:10 -0300

    revisoes

---
+++ REVISAO/revisao.js
@@ -0,0 +1,46 @@
+/*const Map = new Map();
+
+myMap.set('Apple', 'Fruit');
+
+myMap.get(Apple);
+
+myMap.delete('Apple');
+
+myMap.get('Apple');
+
+const pessoas = new Map();
+pessoas.set('Willer', 'Admin');
+pessoas.set('Ingrid', 'Admin');
+pessoas.set('Gru', 'User');
+pessoas.set('Minion', 'User');
+
+function getAdmins(pessoas){
+  let Admins = pessoas.has('Admin')
+
+  for(Admins of pessoas){
+    console.log(Admins)
+  }
+
+};
+
+function getAdmins(pessoas)
+
+
+*/
+
+const myMap = new Map();
+
+myMap.set("Willer", 'Admin');
+
+//console.log(myMap.get('Willer'))
+
+function teste(myMap){
+  let testaAdm = myMap.get('Willer')
+  if(testaAdm == Admin) {
+    console.log('voa')
+  } else {
+    console.log('ero')
+  }
+};
+
+teste()
