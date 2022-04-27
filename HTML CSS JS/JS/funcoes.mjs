cb1d6d6875caed7b239b217ac598f03bffb87cf3
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2022-04-27 09:31:10 -0300

    modulos

---
+++ HTML CSS JS/JS/funcoes.mjs
@@ -0,0 +1,19 @@
+function mostraIdade(nome, idade){
+  return `A idade de ${nome} é ${idade}`
+}
+
+
+function mostraCidade(cidade, caracteristica){
+  return `A cidade de ${cidade} é ${caracteristica}`
+}
+
+/*function dados(){
+  mostraIdade()
+  mostraCidade()
+}*/
+
+export{
+  mostraIdade,
+  mostraCidade,
+}
+
---
+++ HTML CSS JS/JS/index.mjs
@@ -0,0 +1,5 @@
+//import dados from "./funcoes.mjs";
+import {mostraCidade, mostraIdade} from './funcoes.mjs'
+
+console.log(mostraCidade('Guarulhos', 'Densa'))
+console.log(mostraIdade('Willer', '20'))
