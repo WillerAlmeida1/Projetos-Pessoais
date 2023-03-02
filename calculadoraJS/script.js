50f2c7dd91b88284d572540a288c0053f8eae73a
Willer Almeida <contatowilleralmeida@gmail.com>
2023-03-02 19:15:28 -0300

    resolve bug do botao C

--- calculadoraJS/script.js
+++ calculadoraJS/script.js
@@ -5,36 +5,42 @@ const clean = document.querySelector('.c')
 const simbols = ['+', '-', '*', '/']
 let painel = document.querySelector('.painel')
 
-
+//Mostra o valor clicado no "painel"
 const mostraValor = (arr) => {
   arr.forEach(e => {
+    //Adiciona evento de click para todos os botões
     e.addEventListener('click', () => {
       let value = e.innerText
       resultado = painel.innerText += value
-      console.log(`${resultado}`)
+      console.log(resultado)
       return resultado
     })
   })
 }
 
+
+equal.addEventListener('click', () => {
+  fazOperacoes(painel)
+})
+
+
 const fazOperacoes = (nums) => {
   nums = eval(nums)
   painel.innerText = nums
   painel -= equal.innerText
+  console.log(painel)
   return painel
 }
 
-equal.addEventListener('click', () => {
-  fazOperacoes(resultado)
-})
-
-clean.addEventListener('click', () => {
-  painel.innerText -= ''
-})
-
-
+const apaga = () => {
+  clean.addEventListener('click', () => {
+    painel.innerText = ''
+  })
+}
 
 mostraValor(btn)
+apaga()
+
 
 // Fazer uma funcao que recebe os valores de simbolos, as operacoes e outra funcao
 // que percorre a string ve se ela possui um simbolo "x" e dentro disso a gente manda
